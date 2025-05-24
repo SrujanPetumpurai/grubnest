12fff038d6995e4429af3d70cf7b1eaab9e0e6f9
Srujan <srujan.petumpurai@gmail.com>
2025-05-24 23:00:20 +0530

    removed another test folder(what up  with vercel)

--- app/(dashboard)/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import RatingComponent from "@/app/components/RatingComponent"
-
-export default function Test(){
-  return(
-    <div>
-      <RatingComponent></RatingComponent>
-    </div>
-  )
-}
