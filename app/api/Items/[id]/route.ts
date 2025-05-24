3b96478b9238dd22d7594c8b9680292419b99bff
Srujan <srujan.petumpurai@gmail.com>
2025-05-24 22:44:27 +0530

    Added params type as Promise

--- app/api/Items/[id]/route.ts
+++ app/api/Items/[id]/route.ts
@@ -1,26 +1,19 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { NextResponse,NextRequest } from "next/server";
 import { connectToDB } from "@/app/lib/db";
 import { Items } from "@/app/lib/models/user";
 
-export const runtime = "nodejs";
+export async function GET(req: NextRequest,{ params }: { params: Promise<{ id: string }> }) {
+    try {
+       const { id } = await params;
+       await connectToDB();
+        const item = await Items.findOne({ _id: id });
+        console.log(id, "this is params.id")
+        if (!item) {
+            return NextResponse.json({ error: "Item not found" }, { status: 404 });
+        }
 
-export async function GET(
-  request: NextRequest,
-  context: { params: { id: string } }
-) {
-  try {
-    const { id } = context.params;
-    await connectToDB();
-    const item = await Items.findOne({ _id: id });
-
-    if (!item) {
-      return NextResponse.json({ error: "Item not found" }, { status: 404 });
+        return NextResponse.json(item);
+    } catch (error) {
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
     }
-
-    return NextResponse.json(item);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-  }
 }
